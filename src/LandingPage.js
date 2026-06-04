ca03e322a0ee046f0f223a0eb1f5465285f0bb99
Joshua Singh <joshuasingh076@gmail.com>
2026-06-04 18:44:11 -0400

    Update LandingPage.js

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -318,6 +318,12 @@ function LandingPage() {
               >
                 Chess Puzzle
               </Link>
+                <Link 
+                to="https://www.nytimes.com/games/wordle/index.html" 
+                className="block w-full px-6 py-3 bg-lime-600 hover:bg-lime-700 text-white rounded-lg text-center font-medium transition-colors"
+              >
+                NYT Word Games
+              </Link>
               <Link 
                 to="/mastermind" 
                 className="block w-full px-6 py-3 bg-green-600 hover:bg-green-700 text-white rounded-lg text-center font-medium transition-colors"
